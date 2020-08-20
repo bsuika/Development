99a853e2e63832e19b29c5b5de36a39e97c3cbcd
suika <suika@feedbackinfra.com>
2020-08-21 00:59:26 +0530

    DML upload

---
+++ db/DML/DML.sql
@@ -0,0 +1,7 @@
+
+---- DML for movies_info
+
+INSERT INTO db_movies.tbl_movies_info(id,title,category,rating,status)VALUES ('1', 'Virana', 'Horror', '3.0','1');
+INSERT INTO db_movies.tbl_movies_info(id,title,category,rating,status)VALUES ('2', 'Raat', 'Horror', '3.6','1');
+INSERT INTO db_movies.tbl_movies_info(id,title,category,rating,status)VALUES ('3', 'Titanic', 'Hollywood', '3.6','1');
+INSERT INTO db_movies.tbl_movies_info(id,title,category,rating,status)VALUES ('3', 'Aawaj', 'Documentory', '4.8','1');
